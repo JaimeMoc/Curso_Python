79bb231f5dd31c0a6dc721c588b51a2eb6c091a6
Jaime Alberto Suarez Moctezuma <154639506+JaimeMoc@users.noreply.github.com>
2024-12-21 15:14:06 -0600

    Delete DjangoExtras directory

--- DjangoExtras/READme.md
+++
@@ -1,3 +0,0 @@
-# Extras de Django
-
-Aqui guardaremos los ejercicios relacionados con la api Movies que fueron modificados. 
--- DjangoExtras/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-from datetime import datetime
-
-def get_datetime(request):
-   now = datetime.now()
-   response = {"datetime" : now}
-   return JsonResponse(response)
